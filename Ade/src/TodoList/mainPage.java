bc35eb31621523544efa58e8a3f7fe1fbb03f970
bola-dotcom <G00462097@atu.ie>
2026-03-27 22:24:54 +0000

    create part of the main page

---
+++ Ade/src/TodoList/mainPage.java
@@ -0,0 +1,50 @@
+package TodoList;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class mainPage {
+
+	public static void main(String[] args) {
+		//declaring the variables
+		String title;
+		String priority;
+		LocalDate date;
+Scanner scanner = new Scanner(System.in);
+
+while(true)
+{
+	//gives the user the choice
+	System.out.println("\n1.Add Task\n2.ShowTasks\n3.Exit");
+	int choice = scanner.nextInt();
+	scanner.nextLine();
+	
+
+if(choice == 1) {
+	
+	//for the title
+	System.out.println("Title: ");
+	title = scanner.nextLine();
+	
+	
+	//the priority of said task
+	System.out.println("Priority (HIGH/MEDIUM/LOW):");
+	priority = scanner.nextLine();
+	
+	//The due date
+	System.out.println("Due date (YYYY-MM-DD:");
+	 date = LocalDate.parse(scanner.nextLine());
+	
+	
+}
+else if(choice == 2) {
+}
+
+else {
+	
+	break;
+}
+}
+
+}
+	}
+}
