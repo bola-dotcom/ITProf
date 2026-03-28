8a823a935737eda42c79082a13d8455230e3e7a9
bola-dotcom <G00462097@atu.ie>
2026-03-28 22:34:49 +0000

    I added the task class which will be the structure

---
+++ Ade/src/TodoList/Task.java
@@ -0,0 +1,34 @@
+package TodoList;
+import java.time.LocalDate;
+
+public class Task {
+	private String title;
+	private String priority;
+	private LocalDate date;
+	private boolean finished;
+	
+	//constructor
+	public Task(String title, String priority, LocalDate date) {
+		this.title = title;
+		this.priority = priority;
+		this.date = date;
+		this.finished = false;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public LocalDate date() {
+		return date;
+	}
+	public boolean isFinished() {
+		return finished;
+	}
+	
+	public void mark() {
+		this.finished = true;
+	}
+	}
